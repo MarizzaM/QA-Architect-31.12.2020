69667661e438a769fc1f128516acbd1562bf66af
MarizzaM <75089445+MarizzaM@users.noreply.github.com>
2021-01-02 13:29:48 +0200

    Create nested_loop(1-7).java

---
+++ nested_loop(1-7).java
@@ -0,0 +1,106 @@
+package com.company;
+
+import java.lang.annotation.Target;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Scanner s = new Scanner(System.in);
+        Random r = new Random();
+        int number_of_rows, number_of_columns;
+        // 1
+        System.out.println("Task #1");
+        number_of_rows = 5;
+        number_of_columns = 10;
+        int [] arr = new int [number_of_columns];
+        for (int i = 0; i < number_of_rows; i++){
+            for (int j = 0; j < number_of_columns; j++) {
+                arr[j] = j+1;
+            }
+            for (int j = 0; j < number_of_columns; j++){
+                System.out.print(arr[j] + " ");
+            }
+            System.out.println();
+        }
+        // 2
+        System.out.println("Task #2");
+        number_of_rows = 4;
+        String[] days_of_the_week= {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+        for (int i = 0; i < number_of_rows; i++){
+            for (int j = 0; j < days_of_the_week.length; j++){
+                System.out.print(days_of_the_week[j] + " ");
+            }
+            System.out.println();
+        }
+//         3
+        System.out.println("Task #3");
+        int [] arr_numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int [] arr_powers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int i = 0; i < arr_numbers.length; i++){
+            System.out.println(String.format("Powers for number %d", arr_numbers[i]));
+            long pow = 1;
+            for (int j = 0; j < arr_powers.length; j++){
+                pow *= arr_powers[i];
+                if (j <= 4){
+                    System.out.println(String.format("%d^%d = %d", i+1, j+1, pow));
+                }
+            }
+            System.out.println();
+        }
+        // 3
+        System.out.println("Task #3");
+        int [] arr_factorial = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        for (int i = 0; i < arr_factorial.length; i++){
+            long factorial = 1;
+            for (int j = 1; j <= arr_factorial[i]; j++){
+                factorial *= j;
+            }
+            System.out.println(String.format(String.format("Factorial %d! = %d", arr_factorial[i], factorial)));
+        }
+        // 4
+        System.out.println("Task #4");
+        System.out.print("Please enter width of the rectangle: ");
+        number_of_rows = s.nextInt();
+        System.out.print("\nPlease enter length of the rectangle: ");
+        number_of_columns = s.nextInt();
+        for (int i = 0; i < number_of_rows; i++){
+            for(int j = 0; j < number_of_columns; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+//        // 5
+//        System.out.println("Task #5");
+//
+//        System.out.print("Please enter width of the rectangle: ");
+//        number_of_rows = s.nextInt();
+//        System.out.print("\nPlease enter length of the rectangle: ");
+//        number_of_columns = s.nextInt();
+//        String [] width = new String[number_of_rows];
+//        String [] length = new String[number_of_columns];
+//        for (int i = 0; i < number_of_rows; i++){
+//
+//            for(int j = 0; j < number_of_columns; j++){
+//                width[j] = " ";
+////                if (j != 0 || j != number_of_columns-1){
+////                    width[j] = " ";
+////                }
+//                width[0] = "*";
+//                width[number_of_columns-1] = "*";
+//            }
+//
+//        }
+//        for (int i = 0; i < number_of_rows; i++){
+//            for(int j = 0; j < number_of_columns; j++){
+//                System.out.print(width[j]+" ");
+//            }
+//            System.out.println();
+//        }
+        
+    }
+}
